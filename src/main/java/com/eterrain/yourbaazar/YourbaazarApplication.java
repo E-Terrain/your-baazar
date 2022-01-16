b8416a03be0649fb4439d405abb14fcfa7570a96
Anuj <sharma.anuj557@gmail,.com>
2022-01-16 21:17:33 +0530

    YB-01| initial commit for spring boot app

---
+++ src/main/java/com/eterrain/yourbaazar/YourbaazarApplication.java
@@ -0,0 +1,13 @@
+package com.eterrain.yourbaazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourbaazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourbaazarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eterrain/yourbaazar/YourbaazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eterrain.yourbaazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourbaazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
